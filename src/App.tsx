9efbd35340df57631c939fc7394b1145083d3117
ruandqm <ruandqm@gmail.com>
2023-03-08 20:23:26 -0300

    Logon page reallocated

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import { Home } from "./pages/Home/Home";
 import { Movies } from "./pages/Movies/Movies";
 import { LastWatch } from "./pages/LastWatch/LastWatch";
 import { Favorites } from "./pages/Favorites/Favorites";
-import Logon from './components/Logon/index'
+import { Logon } from './pages/Logon/Logon'
 
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Logon />} />
-      <Route path="/Home" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/movies" element={<Movies />} />
       <Route path="/lastWatch" element={<LastWatch />} />
       <Route path="/favorites" element={<Favorites />} />
+      <Route path="/logon" element={<Logon />} />
     </Routes>
   )
 }
