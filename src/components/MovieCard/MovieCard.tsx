1b63fa83104a38366966fae6a82a6576a8b3fbff
ruandqm <ruandqm@gmail.com>
2023-03-10 20:19:22 -0300

    api calls and adapters added

---
+++ src/components/MovieCard/MovieCard.tsx
@@ -0,0 +1,22 @@
+import './style.scss'
+
+export const MovieCard = (props: { data: movie }) => {
+
+    if (props.data != undefined) {
+        const imageUrl = `https://image.tmdb.org/t/p/original/${props.data.poster}`
+        return (
+            <article className='MovieCardContainer' style={{ backgroundImage: `url(${imageUrl})` }}>
+                <div className="movieCardContent">
+                    <div className="genre">{props.data.genre != undefined ? props.data.genre.map((genre: string, index: number) => <span key={index}>{genre}</span>) : null}</div>
+                    <div className="movieInfos">
+                        <span>{`${props.data.runtime}min`}</span>
+                        <h2>{props.data.title}</h2>
+                    </div>
+                </div>
+
+            </article>
+        )
+    } else {
+        return <></>
+    }
+}
