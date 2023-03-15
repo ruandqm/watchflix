f1660bb5999f4652a1a2752ce337a95e9786d5d2
Lucas <lucas75maia@gmail.com>
2023-03-14 23:04:12 -0300

    Fazendo tratamento no login, autorização e criando API dos favoritos

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,16 @@
 import React, { useRef } from 'react'
 import './style.scss'
+import { useSelector } from 'react-redux/es/hooks/useSelector'
 
 export const Navbar = () => {
     const navRef = useRef(null as unknown as HTMLElement);
 
+    const user = useSelector((state: IState) => state.user)
+
     const showNavbar = () => {
         navRef.current.classList.toggle("responsive_nav")
     };
+
     return (
         <header className='NavbarContainer'>
             <div className="index">
@@ -20,20 +24,20 @@ export const Navbar = () => {
                     </ul>
                     <div className="actions">
                         <div className="searchBox">
-
                             <input type="checkbox" id="searchToggle" />
                             <label htmlFor="searchToggle" className="searchToggleIcon"><span className="material-symbols-outlined searchIcon">
                                 search
                             </span>
                             </label>
                             <input type="text" className='searchBoxInput' placeholder='Pesquise um título' />
-
                         </div>
-
                         <div className="userAccount">
-                            {/*  <img className='userImg' src="https://media.licdn.com/dms/image/D4D35AQFLC1gPJx2Miw/profile-framedphoto-shrink_800_800/0/1654873367191?e=1678759200&v=beta&t=0-rglg0dZn5GjDVFrz6Z6eekfK8E-ukZPMqHbyyVYF0" alt="user image" />
-                            <span>Luizão Manzola</span> */}
-                            <a href="/logon">Login</a>
+                            {user.inforUser.length > 0 ? (
+                                <>
+                                    <img className='userImg' src={user.inforUser[2]} alt="user image" />
+                                    <span>{`${user.inforUser[0]} ${user.inforUser[1]}`}</span>
+                                </>
+                            ) : <a href="/logon">Login</a>}
                         </div>
                     </div>
                     <button
@@ -45,8 +49,6 @@ export const Navbar = () => {
                     </button>
                 </nav>
             </div>
-
-
             <button className="nav-btn" onClick={showNavbar}>
                 <span className="material-symbols-outlined">
                     menu
--- src/global.d.ts
+++ src/global.d.ts
@@ -7,3 +7,25 @@ interface movie {
     runtime: number
     genre: string[],
 }
+
+interface IState {
+    user: {
+        inforUser: string[]
+    }
+}
+
+interface ILogon {
+    emailAnddressLogin?: string
+    passwordLogin?: string
+    firstName?: string
+    lastName?: string
+    passwordRegister?: string
+    emailAnddressRegister?: string
+    phone?: string
+    url?: string
+}
+
+interface IAction {
+    type: string
+    payload: any
+}
