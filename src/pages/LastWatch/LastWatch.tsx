91e1c913d9332ad39775abb4caffe93e5e4cf927
Lucas <lucas75maia@gmail.com>
2023-03-16 19:22:03 -0300

    Corrigindo bug dos filmes assistidos e adicionando função Logout

--- src/pages/LastWatch/LastWatch.tsx
+++ src/pages/LastWatch/LastWatch.tsx
@@ -1,29 +1,40 @@
 import './style.scss'
 import { Footer } from '../../components/Footer/Footer'
 import { Navbar } from '../../components/Navbar/Navbar'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { MoviesExpandedSection } from '../../components/MoviesExpandedSection/MoviesExpandedSection'
 import { MoviesNoExpandedSection } from '../../components/MoviesNoExpandedSection/MoviesNoExpandedSection'
 import { NotFound } from '../../components/NotFound/NotFound'
 
 export const LastWatch = () => {
     const [isExpanded, setIsExpanded] = useState(false)
+    const [data, setData] = useState<movie[]>([])
 
     const localData = window.localStorage.getItem('lastWatchList')
 
-    let arrayMovies = JSON.parse(localData != null ? localData : '')
+    let arrayMovies = JSON.parse(localData != null ? localData : '[]')
 
-    let reduced: movie[] = [];
+    function setMoviesAssisted() {
+        if (arrayMovies.length > 0) {
+            let reduced: movie[] = [];
 
-    arrayMovies.forEach((item: movie) => {
-        let duplicated = reduced.findIndex(redItem => {
-            return item.id == redItem.id;
-        }) > -1;
+            arrayMovies.forEach((item: movie) => {
+                let duplicated = reduced.findIndex(redItem => {
+                    return item.id == redItem.id;
+                }) > -1;
 
-        if (!duplicated) {
-            reduced.push(item);
+                if (!duplicated) {
+                    reduced.push(item);
+                }
+            });
+
+            setData(reduced)
         }
-    });
+    }
+
+    useEffect(() => {
+        setMoviesAssisted()
+    }, [])
 
     return (
         <div className="MoviesPageContainer">
@@ -37,8 +48,8 @@ export const LastWatch = () => {
                 </section>
 
                 {localData == null && <NotFound />}
-                {!isExpanded && localData != null ? <MoviesNoExpandedSection data={reduced} /> : null}
-                {isExpanded && localData != null ? <MoviesExpandedSection data={reduced} /> : null}
+                {!isExpanded && data != null ? <MoviesNoExpandedSection data={data} /> : null}
+                {isExpanded && data != null ? <MoviesExpandedSection data={data} /> : null}
             </div>
             <Footer />
         </div>
